6ba7915f1850447ab7c8ad997dfae3e1b0ae9135
Satyam-code143 <Satyam-code143@github.com>
2021-05-13 22:10:29 +0530

    done

--- FrontEnd/plasma_donation/src/pages/Dashboard/DashboardDesktop.jsx
+++ FrontEnd/plasma_donation/src/pages/Dashboard/DashboardDesktop.jsx
@@ -6,6 +6,7 @@ import Lottie from "lottie-react";
 import Animation from "../../resources/json/lottieAnimation1.json";
 import Aos from "aos";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 function DashboardDesktop(props) {
   const classes = useStyles();
@@ -14,24 +15,64 @@ function DashboardDesktop(props) {
     Aos.init({ duration: 1000 });
   }, []);
 
+  const DonarButton = React.forwardRef((props, ref) => (
+    <Button
+      variant="contained"
+      className={classes.donorButton}
+      component={motion.a}
+      whileHover={{
+        scale: 1.2,
+        transition: { duration: 0.3 },
+      }}
+      {...props}
+    >
+      Donor
+    </Button>
+  ));
+  const CollectorButton = React.forwardRef((props, ref) => (
+    <Button
+      variant="outlined"
+      className={classes.collectorButton}
+      component={motion.a}
+      whileHover={{
+        scale: 1.2,
+        transition: { duration: 0.3 },
+      }}
+    >
+      Collector
+    </Button>
+  ));
+  const LoginButton = React.forwardRef((props, ref) => (
+    <Button
+      variant="contained"
+      className={classes.loginButton}
+      component={motion.div}
+      whileHover={{
+        scale: 1.2,
+        transition: { duration: 0.3 },
+      }}
+    >
+      Login
+    </Button>
+  ));
   return (
     <Container maxWidth="lg" className="mainContainer">
       <div className={classes.topContainer}></div>
-      <div className={classes.dashboardHeaderContainer}>
+      <div className={classes.commonHeaderContainer}>
         <Lottie
           animationData={Animation}
           style={{ width: 150, height: 100 }}
           data-aos="fade-right"
         />
-        <div className={classes.headerCompartment} data-aos="fade-right">
+        <div className={classes.headerTextcontainer} data-aos="fade-right">
           <Typography
-            variant="h3"
+            variant="h4"
             color="initial"
             className={classes.headerTypo}
           >
             Dashboard
           </Typography>
-          <div className={classes.lineHeight}></div>
+          <div className={classes.lineHeightyellow}></div>
         </div>
       </div>
       <div className={classes.authContainer}>
@@ -44,28 +85,8 @@ function DashboardDesktop(props) {
           Register as
         </Typography>
         <div className={classes.buttonContainer} data-aos="fade-up">
-          <Button
-            variant="contained"
-            className={classes.donorButton}
-            component={motion.div}
-            whileHover={{
-              scale: 1.2,
-              transition: { duration: 0.3 },
-            }}
-          >
-            Donor
-          </Button>
-          <Button
-            variant="outlined"
-            className={classes.collectorButton}
-            component={motion.div}
-            whileHover={{
-              scale: 1.2,
-              transition: { duration: 0.3 },
-            }}
-          >
-            Collector
-          </Button>
+          <Link to="/donorRegistration" component={DonarButton} />
+          <Link to="/collectorRegistration" component={CollectorButton} />
         </div>
         <Typography
           variant="h5"
@@ -76,17 +97,7 @@ function DashboardDesktop(props) {
           Already a member?
         </Typography>
         <div data-aos="fade-up">
-          <Button
-            variant="contained"
-            className={classes.loginButton}
-            component={motion.div}
-            whileHover={{
-              scale: 1.2,
-              transition: { duration: 0.3 },
-            }}
-          >
-            Login
-          </Button>
+          <Link to="/loginPage" component={LoginButton} />
         </div>
       </div>
       <div className={classes.infoContainer}>
