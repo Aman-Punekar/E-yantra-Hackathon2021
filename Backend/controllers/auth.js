99686e8aee48286bd32bd49948f9896acbab9ee5
Varun Joshi <varunj442@gmail.com>
2021-05-18 22:38:19 +0530

    added patient login

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
-const crypto = require('crypto');   // for encryption
-const jwt = require('jsonwebtoken');  // issuing jwts
+const crypto = require('crypto'); // for encryption
+const jwt = require('jsonwebtoken'); // issuing jwts
 const ACCOUNT_SID = 'ACe76bd995e206ab993cdbe06d22004e73';
 const AUTH_TOKEN = '796ec1d88e088c5e177594f5e73ba789';
 const client = require('twilio')(ACCOUNT_SID, AUTH_TOKEN); // configuring twilio
-
-
 
 // environment variables
 const JWT_AUTH_TOKEN = process.env.JWT_AUTH_TOKEN;
@@ -23,30 +21,31 @@ appended creating fullhash.
     A json object containing the phone and fullhash is send to the client.
 
 */
-const sendOtp = (req,res) => {
-    
-    
-    const phone = req.body.phone;
-    const otp = Math.floor(100000 + Math.random()*900000); // generating otp
-    const ttl = 30*60*1000;
-    const expires = Date.now() +ttl; // expiry time
-    const data = `${phone}.${otp}.${expires}`;
-    const hash = crypto.createHmac('sha256', smsKey).update(data).digest('hex');
-    const fullhash = `${hash}.${expires}`;
+const sendOtp = (req, res) => {
+	const phone = req.body.phone;
+	const otp = Math.floor(100000 + Math.random() * 900000); // generating otp
+	const ttl = 30 * 60 * 1000;
+	const expires = Date.now() + ttl; // expiry time
+	const data = `${phone}.${otp}.${expires}`;
+	const hash = crypto.createHmac('sha256', smsKey).update(data).digest('hex');
+	const fullhash = `${hash}.${expires}`;
 
-    
-    // the following setup is the twilio configuration
+	// the following setup is the twilio configuration
 
-    client.messages
-    .create({
-        body: `your otp is ${otp}`,
-        from: '+14793455072',
-        to: phone
-    })
-    .then((message) => console.log('Success'))
-    .catch((err) => console.log(err));
+	client.messages
+		.create({
+			body: `your otp is ${otp}`,
+			from: '+14793455072',
+			to: phone
+		})
+		.then((message) => console.log('Success'))
+		.catch((err) => console.log(err));
 
-    res.status(200).send({phone, hash: fullhash, otp})
+	res.status(200).send({
+		phone,
+		hash: fullhash,
+		otp
+	});
 };
 
 /*
@@ -62,30 +61,46 @@ for other routes. Along with accessToken, refreshToken is also sent for keeping 
 Otherwise 400 status is sent stating that otp is incorrect    
 
 */
-const verifyOtp  = (req,res) => {
-    const phone = req.body.phone;
-    const hash = req.body.hash;
-    const otp = req.body.otp;
-    let [hashValue, expires] = hash.split('.');
+const verifyOtp = (req, res) => {
+	const phone = req.body.phone;
+	const hash = req.body.hash;
+	const otp = req.body.otp;
+	let [ hashValue, expires ] = hash.split('.');
 
-    let now = Date.now();
+	let now = Date.now();
 
-    if(now > parseInt(expires)){
-        return res.status(504).send({msg:'timeout please try again'});
-    }
-    const data = `${phone}.${otp}.${expires}`;
-    const newCalculatedHash = crypto.createHmac('sha256', smsKey).update(data).digest('hex');
+	if (now > parseInt(expires)) {
+		return res.status(504).send({
+			msg: 'timeout please try again'
+		});
+	}
+	const data = `${phone}.${otp}.${expires}`;
+	const newCalculatedHash = crypto.createHmac('sha256', smsKey).update(data).digest('hex');
 
-    if (newCalculatedHash === hashValue){
-        //return res.status(202).send({msg:'User confirmed'});
+	if (newCalculatedHash === hashValue) {
+		//return res.status(202).send({msg:'User confirmed'});
 
-        
-        const accessToken = jwt.sign({data:phone}, JWT_AUTH_TOKEN, {expiresIn: '1d'});
-        const refreshToken = jwt.sign({data:phone}, JWT_REFRESH_TOKEN, {expiresIn: '1y'});
-        refreshTokens.push(refreshToken);
+		const accessToken = jwt.sign(
+			{
+				data: phone
+			},
+			JWT_AUTH_TOKEN,
+			{
+				expiresIn: '1d'
+			}
+		);
+		const refreshToken = jwt.sign(
+			{
+				data: phone
+			},
+			JWT_REFRESH_TOKEN,
+			{
+				expiresIn: '1y'
+			}
+		);
+		refreshTokens.push(refreshToken);
 
-
-        res
+		res
 			.status(202)
 			.cookie('accessToken', accessToken, {
 				expires: new Date(new Date().getTime() + 86400 * 1000),
@@ -97,51 +112,81 @@ const verifyOtp  = (req,res) => {
 				sameSite: 'strict',
 				httpOnly: true
 			})
-			.cookie('authSession', true, { expires: new Date(new Date().getTime() + 86400 * 1000), sameSite: 'strict' })
+			.cookie('authSession', true, {
+				expires: new Date(new Date().getTime() + 86400 * 1000),
+				sameSite: 'strict'
+			})
 			.cookie('refreshTokenID', true, {
 				expires: new Date(new Date().getTime() + 31557600000),
-                sameSite: 'strict'
-				
+				sameSite: 'strict'
 			})
-			.send({ msg: 'Device verified' });
-    }else{
-        return res.status(400).send({verification: false, msg: 'Incorrect OTP'})
-    }
+			.send({
+				msg: 'Device verified'
+			});
+	} else {
+		return res.status(400).send({
+			verification: false,
+			msg: 'Incorrect OTP'
+		});
+	}
 };
 
-const refresh = (req,res,next)=>{
-    const refreshToken = req.cookies.refreshToken;
-    if(!refreshToken) return res.status(403).send({msg:'refresh token not found, please login again'});
-    if(!refreshTokens.includes(refreshToken)) return res.status(403).send({msg:'refresh token blocked, please login again'});
+const refresh = (req, res, next) => {
+	const refreshToken = req.cookies.refreshToken;
+	if (!refreshToken)
+		return res.status(403).send({
+			msg: 'refresh token not found, please login again'
+		});
+	if (!refreshTokens.includes(refreshToken))
+		return res.status(403).send({
+			msg: 'refresh token blocked, please login again'
+		});
 
-    jwt.verify(refreshToken, JWT_REFRESH_TOKEN,(err, phone) => {
-        if(!err){
-            const accessToken = jwt.sign({data:phone}, JWT_AUTH_TOKEN, {expiresIn: '1d'});
-            res
-            .status(202)
-            .cookie('accessToken', accessToken, {
-                expires: new Date(new Date().getTime()+ 30*1000),
-                sameSite: 'strict',
-                httpOnly: true
-            })
-            .cookie('authSession', true, {
-                expires: new Date(new Date().getTime()+ 30*1000),
-                
-            }).send({previousSessionExpiry:true, success:true});
-        }else{
-            return res.status(403).send({success:false, msg:'Invalid Refresh Token'});
-        }
-    })
-    
+	jwt.verify(refreshToken, JWT_REFRESH_TOKEN, (err, phone) => {
+		if (!err) {
+			const accessToken = jwt.sign(
+				{
+					data: phone
+				},
+				JWT_AUTH_TOKEN,
+				{
+					expiresIn: '1d'
+				}
+			);
+			res
+				.status(202)
+				.cookie('accessToken', accessToken, {
+					expires: new Date(new Date().getTime() + 30 * 1000),
+					sameSite: 'strict',
+					httpOnly: true
+				})
+				.cookie('authSession', true, {
+					expires: new Date(new Date().getTime() + 30 * 1000)
+				})
+				.send({
+					previousSessionExpiry: true,
+					success: true
+				});
+		} else {
+			return res.status(403).send({
+				success: false,
+				msg: 'Invalid Refresh Token'
+			});
+		}
+	});
 };
 
-const logout = (req, res)=>{
-    res
-        .clearCookie('refreshToken')
-        .clearCookie('accessToken')
-        .clearCookie('authSession')
-        .clearCookie('refreshTokenID')
-        .send('User logged out')
+const logout = (req, res) => {
+	res.clearCookie('refreshToken').clearCookie('accessToken').clearCookie('authSession').clearCookie('refreshTokenID');
+	// .send('User logged out')
+	res.json({
+		message: 'User signout successfully'
+	});
 };
 
-module.exports = {sendOtp,verifyOtp,refresh,logout};
+module.exports = {
+	sendOtp,
+	verifyOtp,
+	refresh,
+	logout
+};
--- Backend/models/Donor.js
+++ Backend/models/Donor.js
@@ -1,76 +1,72 @@
 // model require only the mongoose and schema
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const donorSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true,
-        maxlength: 32,
-        trim: true
-        
-      },
-    lastName :{
-        type: String,
-        required: true,
-        maxlength: 32,
-        trim: true
-      },
-    age: {
-        type: Number,
-        required : true,
-        maxlength : 3,
-        trim : true
-    },
-    gender: {
-        type: String,
-    },
-    mobileNo:{
-        type : Number,
-        maxlength:10
-    },
-    alternateNo:{
-        type : Number,
-        maxlength:10
-    },
-    
-    address: {
-        lane : {
-            type : String,
-            trim: true
-        },
-        city: {
-            type : String,
-            trim: true,
-            required:true
-        },
-        district : {
-            type: String,
-            trim: true
-        },
-        state:{
-            type : String,
-            trim: true,
-            required:true
-        },
-        pinCode: {
-            type:Number,
-            required:true,
-            maxlength : 6
-        }
-    },
-    
-    bloodGroup : {
-        type: String,
-        required: true
-    },
-    profile:{
-        data: Buffer,
-        contentType:String
-    },
+	firstName: {
+		type: String,
+		required: true,
+		maxlength: 32,
+		trim: true
+	},
+	lastName: {
+		type: String,
+		required: true,
+		maxlength: 32,
+		trim: true
+	},
+	age: {
+		type: Number,
+		required: true,
+		maxlength: 3,
+		trim: true
+	},
+	gender: {
+		type: String
+	},
+	mobileNo: {
+		type: Number,
+		maxlength: 10,
+		unique: true
+	},
+	alternateNo: {
+		type: Number,
+		maxlength: 10
+	},
+	address: {
+		lane: {
+			type: String,
+			trim: true
+		},
+		city: {
+			type: String,
+			trim: true,
+			required: true
+		},
+		district: {
+			type: String,
+			trim: true
+		},
+		state: {
+			type: String,
+			trim: true,
+			required: true
+		},
+		pinCode: {
+			type: Number,
+			required: true,
+			maxlength: 6
+		}
+	},
 
-
+	bloodGroup: {
+		type: String,
+		required: true
+	},
+	profile: {
+		data: Buffer,
+		contentType: String
+	}
 });
 
-module.exports = mongoose.model("Donor1", donorSchema);
+module.exports = mongoose.model('Donor1', donorSchema);
